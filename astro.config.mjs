2540136378eed22cf6f626c31a93712a67fc530e
Everthon de Souza Oliveira <everthonsol@yahoo.com.br>
2024-12-18 14:47:33 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  site: 'https://seu-usuario.github.io/', // Substitua pelo seu usuário do GitHub
-  base: '/nome-do-repositorio/',          // Caminho base do seu repositório
-  outDir: 'dist',                         // Pasta de saída após o build
+  output: 'static',
+  adapter: netlify(),
+  site: 'https://everthonso.github.io',
+  base: '/', // Remove the repository name from base
 });
